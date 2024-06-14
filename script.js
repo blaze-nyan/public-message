440ca1693765fbb2ffa3b72b52cc97f54767d971
nyan-lin-aung0-hero <channelpieces@gmail.com>
2024-06-14 22:33:56 +0700

    clean code

--- script.js
+++ script.js
@@ -3,38 +3,36 @@ const messageTextFieldEl = document.getElementById("message"); // this is text f
 const publishBtnEl = document.getElementById("publish"); // this is button
 const messagesContainerEl = document.getElementById("messages");
 
-// const deleteBtnEl = document.querySelectorAll(".message:hover .delete-btn");
-//publish message
-publishBtnEl.addEventListener("click", function () {
-  deployNewmessage();
-});
+const messagesArray = [];
+
+publishBtnEl.addEventListener("click", deployNewmessage);
 
 //function for creating new messages
-function createNewmessage(message) {
-  let newMessage = document.createElement("div"); // creating div for new message
-  newMessage.classList.add("message"); // adding classname for predefined style
-  newMessage.textContent = message; // inserting message in the message div
-  messagesContainerEl.appendChild(newMessage); // inserting the whole new div into the container
-
-  // creating delete button
-  let newDeleteBtn = document.createElement("button");
-  newDeleteBtn.classList.add("delete-btn");
-  newDeleteBtn.innerHTML = "<img src='images/Trash.png'/> ";
-  newMessage.appendChild(newDeleteBtn);
-  newDeleteBtn.addEventListener("click", function () {
-    console.log("clicked");
-    newMessage.textContent = "Damn";
-  });
-
-  // reseting textarea empty
-  resetText(messageTextFieldEl);
+function createNewmessage() {
+  let newMessage = ``;
+  for (let [i, message] of messagesArray.entries()) {
+    newMessage += `
+      <div class="message" id=${i}>
+        <span>${message} </span>
+        <button class="delete-btn" id=${i}>
+          <img src='images/Trash.png'/>
+        </button>
+      </div>`;
+  }
+  return newMessage;
 }
+
 // new message deploy function
 function deployNewmessage() {
-  let textMessage = messageTextFieldEl.value; //getting value from textarea
+  let textMessage = messageTextFieldEl.value;
+  console.log(typeof messageTextFieldEl.value, messageTextFieldEl.value);
+  resetText(messageTextFieldEl); //getting value from textarea
+  console.log(textMessage);
   // checking whether textfiled is empty or not
-  if (textMessage !== "") {
-    createNewmessage(textMessage);
+  if (textMessage[0] !== "") {
+    messagesArray.push(textMessage);
+    render();
+    console.log(typeof textMessage);
   } else {
     alert("no message yet");
   }
@@ -44,21 +42,15 @@ function resetText(element) {
   element.value = "";
 }
 
-// deleteBtnEl.forEach((btn) => {
-//   btn.addEventListener("click", function () {
-//     console.log("clicked");
-//   });
-// });
-
+function render() {
+  if (messagesArray[0]) {
+    messagesContainerEl.innerHTML = "";
+    messagesContainerEl.innerHTML = createNewmessage();
+  }
+}
 // enter keydown function
 messageTextFieldEl.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     deployNewmessage();
   }
 });
-
-// messageDiv.addEventListener('click', (event) => {
-//   if (event.target.classList.contains('delete-btn')) {
-//     console.log('Button clicked!');
-//   }
-// });

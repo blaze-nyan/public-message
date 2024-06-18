9b32a951d66be4385e0b4d057a3e3dcb148012ec
nyan-lin-aung0-hero <channelpieces@gmail.com>
2024-06-18 21:48:03 +0700

    can delete messages individually

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const messageTextFieldEl = document.getElementById("message"); // this is text field
-
+const messageTextFieldEl = document.getElementById("message-text"); // this is text field
 const publishBtnEl = document.getElementById("publish"); // this is button
 const messagesContainerEl = document.getElementById("messages");
-
+let key = 0;
 const messagesArray = [];
 
 publishBtnEl.addEventListener("click", deployNewmessage);
@@ -10,12 +9,12 @@ publishBtnEl.addEventListener("click", deployNewmessage);
 //function for creating new messages
 function createNewmessage() {
   let newMessage = ``;
-  for (let [i, message] of messagesArray.entries()) {
+  for (let message of messagesArray) {
     newMessage += `
-      <div class="message" id=${i}>
-        <span>${message} </span>
-        <button class="delete-btn" id=${i}>
-          <img src='images/Trash.png'/>
+      <div class="message" id=${message.key}>
+        <span>${message.textMessage} </span>
+        <button class="delete-btn" data-message=${message.key}>
+          <i class="fa-solid fa-trash-can" data-message=${message.key}></i>
         </button>
       </div>`;
   }
@@ -24,16 +23,18 @@ function createNewmessage() {
 
 // new message deploy function
 function deployNewmessage() {
-  let textMessage = messageTextFieldEl.value;
-  console.log(typeof messageTextFieldEl.value, messageTextFieldEl.value);
-  resetText(messageTextFieldEl); //getting value from textarea
-  console.log(textMessage);
-  // checking whether textfiled is empty or not
-  if (textMessage[0] !== "") {
-    messagesArray.push(textMessage);
+  let userMessage = messageTextFieldEl.value;
+  textMessage = userMessage.trim();
+  if (textMessage !== "") {
+    console.log(textMessage);
+    resetText(messageTextFieldEl); //getting value from textarea
+    // checking whether textfiled is empty or not
+
+    messagesArray.push({ key, textMessage });
+    key++;
     render();
-    console.log(typeof textMessage);
   } else {
+    resetText(messageTextFieldEl);
     alert("no message yet");
   }
 }
@@ -46,11 +47,31 @@ function render() {
   if (messagesArray[0]) {
     messagesContainerEl.innerHTML = "";
     messagesContainerEl.innerHTML = createNewmessage();
+  } else {
+    messagesContainerEl.innerHTML = "";
   }
 }
+
 // enter keydown function
 messageTextFieldEl.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
+    event.preventDefault();
     deployNewmessage();
+  }
+});
+
+//testing
+
+document.addEventListener("click", function (e) {
+  let exactMessageIndex;
+  if (e.target.dataset.message) {
+    messagesArray.forEach(function (message) {
+      if (e.target.dataset.message == message.key) {
+        exactMessageIndex = messagesArray.indexOf(message);
+        messagesArray.splice(exactMessageIndex, 1);
+        console.log(messagesArray);
+        render();
+      }
+    });
   }
 });
